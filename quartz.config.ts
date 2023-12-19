2b6962bdddba7054d9adf898395e5557a6570aaf
Alexander <alexanderweichart@fastmail.com>
2023-12-19 09:23:06 +0100

    init

---
+++ quartz.config.ts
@@ -0,0 +1,78 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "アレックスの庭",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    baseUrl: "quartz.jzhao.xyz",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "created",
+    theme: {
+      typography: {
+        header: "Noto Serif JP",
+        body: "Feijoa",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#ffffff", // main background remains white
+          lightgray: "#e5e5e5",
+          gray: "#b8b8b8",
+          darkgray: "#636363",
+          dark: "#2b2b2b",
+          secondary: "#808c27", // changed to a green shade
+          tertiary: "#6a7326", // changed to a lighter green
+          highlight: "rgba(118, 185, 71, 0.10)", // less intense green-tinted highlight
+        },
+
+        darkMode: {
+          light: "#161618",
+          lightgray: "#393639",
+          gray: "#646464",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#6daa6c", // changed to a green shade
+          tertiary: "#76b947", // consistent with light mode tertiary
+          highlight: "rgba(118, 185, 71, 0.15)", // green-tinted highlight
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.TableOfContents(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "filesystem"], // you can add 'git' here for last modified from Git but this makes the build slower
+      }),
+      Plugin.SyntaxHighlighting(),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.Description(),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources({ fontOrigin: "googleFonts" }),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
---
+++ quartz/components/PageTitle.tsx
@@ -0,0 +1,25 @@
+import { pathToRoot } from "../util/path"
+import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
+
+function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
+  const title = cfg?.pageTitle ?? "Untitled Quartz"
+  const baseDir = pathToRoot(fileData.slug!)
+  return (
+    <h1 class={`page-title ${displayClass ?? ""}`}>
+      <div id="page-title-icon"></div>
+      <a id={"page-title-link"} href={baseDir}>
+        <ruby id="page-title-link-furi">
+          <rt>Alex' Garden</rt>{title}
+        </ruby>
+      </a>
+    </h1>
+  )
+}
+
+PageTitle.css = `
+.page-title {
+  margin: 0;
+}
+`
+
+export default (() => PageTitle) satisfies QuartzComponentConstructor
