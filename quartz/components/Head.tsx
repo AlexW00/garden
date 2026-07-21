a983a4f445df613ec6d76e06117b167cd68fce6f
Alexander Weichart <55558407+AlexW00@users.noreply.github.com>
2026-07-21 12:06:35 +0900

    chore(seo): add Google Search Console verification meta tag

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -44,6 +44,10 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+        <meta
+          name="google-site-verification"
+          content="39Me82u9XPQQkVT8LBb6kd4-7JkFtCDq7DloPVsVdTU"
+        />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
